9be35c982eb284f7ed9987803f75e804e3ffdbef
Arthur Toussaint <arthurtoussaint@free.fr>
2019-11-04 14:20:28 -0500

    Cleaning

--- collect/past.py
+++ collect/past.py
@@ -1,27 +1,11 @@
-import json
 import logging
-import curses
-import signal
 import sys
-import glob
-import math
-import json
-import random
 import time
 from datetime import datetime
 import networkx as nx
 from colorama import Fore, Back, Style, init
-import matplotlib.pyplot as plt
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
 import ast
-
-AS_TO_DISPLAY = 513 # CERN
-AS_TO_DISPLAY = 15169 # Google
-
-DEGREE_DISPLAY_LABEL = 50
-
-MESSAGES_TO_GATHER = 1e5
-#MESSAGES_TO_GATHER = 1e2
 
 SAVE_INTERVAL = 30
 #SAVE_INTERVAL = 10
@@ -29,85 +13,10 @@ SAVE_INTERVAL = 30
 START_TIMESTAMP = 1438417216
 RECORD_PERIOD = 60*2
 
-DISPLAY = False
-SAVE = True
-
-def extract_core(ASNs):
-	cont = True
-	G = ASNs.copy()
-	while cont:
-		cont = False
-		for node, degree in G.copy().degree():
-			if degree <= 2:
-				G.remove_node(node)
-				cont = True
-	return G.nodes()
-
-def wiggle(t, r):
-	a,b = t
-	a += random.uniform(-r,r)
-	b += random.uniform(-r,r)
-	return (a,b)
-
-def restore_terminal(signal, frame):
-	curses.echo()
-	curses.nocbreak()
-	curses.endwin()
-	sys.exit(0)
-
 init() # Init colors
-
-# Init curses
-#stdscr = curses.initscr()
-#curses.noecho()
-#curses.cbreak()
-
-
-signal.signal(signal.SIGINT, restore_terminal)
-signal.signal(signal.SIGTERM, restore_terminal)
-
-# Create ASN to countries mapping
-AS_countries = {}
-AS_files = glob.glob("./db/*_asn")
-for AS_file in AS_files:
-	with open(AS_file, "r") as AS_fd:
-		line = AS_fd.readline()
-		while line:
-			AS = int(line[2:].strip())
-			country = AS_file.split("/")[-1][0:2].upper()
-			AS_countries[AS] = country
-
-			line = AS_fd.readline()
-
-country_continent = {}
-with open("continents.json", "r") as continent_file:
-	continents = json.load(continent_file)
-	for continent in continents:
-		country_continent[continent["Two_Letter_Country_Code"]] = continent["Continent_Name"]
-
-colors = {
-	"Antartica": "skyblue",
-	"Asia": "yellow",
-	"Africa": "violet",
-	"Europe": "blue",
-	"North America": "tan",
-	"Oceania": "seagreen",
-	"South America": "orange"
-}
-
-initial_positions = {
-	"Antartica": (0.1, 0.1),
-	"Asia": (0.9, 0.5),
-	"Africa": (0.5, 0.1),
-	"Europe": (0.5, 0.9),
-	"North America": (0.1, 0.9),
-	"Oceania": (0.9, 0.1),
-	"South America": (0.1,0.5)
-}
 
 # Init logging
 logging.basicConfig(level=logging.WARNING)
-
 
 # Create stream
 stream = BGPStream()
@@ -122,7 +31,7 @@ ASNs = nx.DiGraph()
 
 messages_recieved = 0
 
-last_time_saved = 1438417216
+last_time_saved = START_TIMESTAMP
 
 while(stream.get_next_record(rec)):
 	if rec.status != "valid":
@@ -168,49 +77,9 @@ while(stream.get_next_record(rec)):
 		elem = rec.get_next_elem()
 
 	# Save
-	if SAVE and rec.time - last_time_saved >= SAVE_INTERVAL:
+	if rec.time - last_time_saved >= SAVE_INTERVAL:
 		filename = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 		print("Saving graph,", len(ASNs.nodes), "nodes...", end="")
 		nx.readwrite.gml.write_gml(ASNs, f"out/{filename}.gml.gz", stringizer=lambda x: str(list(x)))
 		print(" Saved")
 		last_time_saved = rec.time
-
-
-# Display
-if DISPLAY:
-	print(len(ASNs.nodes()), "nodes to display")
-
-	print("Collection complete")
-	print("Calculating core")
-
-	core = extract_core(ASNs)
-
-	print("Positioning")
-
-	pos = {}
-	for node in ASNs.nodes():
-		pos[node] = wiggle((0.5, 0.5) if node in core else initial_positions[country_continent[AS_countries[node]]] if node in AS_countries else (0.5,0.5), 0.1)
-
-	pos=nx.spring_layout(ASNs, pos=pos)
-	#pos=nx.kamada_kawai_layout(ASNs, pos=pos)
-	#pos=nx.spectral_layout(ASNs)
-	labels = {}
-	
-	print("Assigning labels")
-	for node, degree in ASNs.degree():
-		if degree > DEGREE_DISPLAY_LABEL:
-			labels[node] = node
-
-	print("Drawing")
-	nx.draw(ASNs, pos, arrowstyle="->", with_labels=False, node_color=["r" if x in core else colors[country_continent[AS_countries[x]]] if x in AS_countries else "black" for x in ASNs.nodes()], node_size=[3*x**(2/3) for _, x in ASNs.degree()], width=0.3, edge_color="grey", alpha=0.7)
-
-	print("drawing labels")
-	nx.draw_networkx_labels(ASNs, pos, labels, font_size=8)
-
-	plt.show()
-
-	print(f"AS {AS_TO_DISPLAY} has {len(list(nx.all_neighbors(ASNs, AS_TO_DISPLAY)))} neighbors : " + ', '.join(map(str, [x for _, x in nx.all_neighbors(ASNs, AS_TO_DISPLAY)])))
-
-	exit(0)
-elif DISPLAY:
-	print(int((messages_recieved/MESSAGES_TO_GATHER)*100), "% complete", end="\r")
