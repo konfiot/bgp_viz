26b379be505a7df031d5ab370b8f9302e88174e6
konfiot <arthur.toussaint@polytechnique.edu>
2019-11-09 13:34:30 -0500

    Add the visualization script to the main script

--- visualize/graph.py
+++ visualize/graph.py
@@ -2,13 +2,12 @@
 
 import glob
 import networkx as nx
-import sys
 import matplotlib.pyplot as plt
 import json
 import random
 import numpy as np
-DEGREE_DISPLAY_LABEL = 100
-DPI = 600 # tu te calmes
+#DEGREE_DISPLAY_LABEL = 100
+#DPI = 600 # tu te calmes
 
 def extract_core(ASNs):
 	cont = True
@@ -27,26 +26,26 @@ def wiggle(t, r):
 	b += random.uniform(-r,r)
 	return (a,b)
 
-# Create ASN to countries mapping
-AS_countries = {}
-AS_files = glob.glob("./db/*_asn")
-for AS_file in AS_files:
-	with open(AS_file, "r") as AS_fd:
-		line = AS_fd.readline()
-		while line:
-			AS = int(line[2:].strip())
-			country = AS_file.split("/")[-1][0:2].upper()
-			AS_countries[AS] = country
-
+def do_display(args):
+	AS_countries = {}
+	AS_files = glob.glob("./db/*_asn")
+	for AS_file in AS_files:
+		with open(AS_file, "r") as AS_fd:
 			line = AS_fd.readline()
+			while line:
+				AS = int(line[2:].strip())
+				country = AS_file.split("/")[-1][0:2].upper()
+				AS_countries[AS] = country
 
-country_continent = {}
-with open("continents.json", "r") as continent_file:
-	continents = json.load(continent_file)
-	for continent in continents:
-		country_continent[continent["Two_Letter_Country_Code"]] = continent["Continent_Name"]
+				line = AS_fd.readline()
 
-colors = {
+	country_continent = {}
+	with open("continents.json", "r") as continent_file:
+		continents = json.load(continent_file)
+		for continent in continents:
+			country_continent[continent["Two_Letter_Country_Code"]] = continent["Continent_Name"]
+
+	colors = {
 		"Antartica": "skyblue",
 		"Asia": "yellow",
 		"Africa": "violet",
@@ -54,9 +53,9 @@ colors = {
 		"North America": "tan",
 		"Oceania": "seagreen",
 		"South America": "orange"
-		}
+	}
 
-initial_positions = {
+	initial_positions = {
 		"Antartica": (0.1, 0.1),
 		"Asia": (0.9, 0.5),
 		"Africa": (0.5, 0.1),
@@ -64,59 +63,53 @@ initial_positions = {
 		"North America": (0.1, 0.9),
 		"Oceania": (0.9, 0.1),
 		"South America": (0.1,0.5)
-		}
+	}
 
-if len(sys.argv) < 2:
-	print(f"usage: {sys.argv[0]} filename.gml[.gz]")
-	exit(-1)
+	print(f"Reading {args.file}")
 
-print(f"Reading {sys.argv[1]}")
+	ASNs = nx.convert_node_labels_to_integers(nx.read_gml(args.file))
 
-ASNs = nx.convert_node_labels_to_integers(nx.read_gml(sys.argv[1]))
+	print(len(ASNs.nodes()), "nodes to display")
 
-print(len(ASNs.nodes()), "nodes to display")
-
-print("Collection complete")
-print("Calculating core")
-
-core = extract_core(ASNs)
+	core = extract_core(ASNs)
 
 # get adj matrix
-A = nx.adjacency_matrix(ASNs)
+	A = nx.adjacency_matrix(ASNs)
 
-print('average degree : ', np.mean(np.sum(A, axis = 1)))
+	print('average degree : ', np.mean(np.sum(A, axis = 1)))
 
 # replace weighted entries with 1 for adj visualization
-A[A>0] = 1
+	A[A>0] = 1
 
-plt.imshow(A.todense())
-plt.title('Adjacency Matrix (unweighted)')
-plt.show()
-plt.close()
+	plt.imshow(A.todense())
+	plt.title('Adjacency Matrix (unweighted)')
+	plt.show()
+	plt.close()
 
-print("Positioning")
+	print("Positioning")
 
-pos = {}
-for node in ASNs.nodes():
-	pos[node] = wiggle((0.5, 0.5) if node in core else initial_positions[country_continent[AS_countries[node]]] if node in AS_countries else (0.5,0.5), 0.1)
+	pos = {}
+	for node in ASNs.nodes():
+		pos[node] = wiggle((0.5, 0.5) if node in core else initial_positions[country_continent[AS_countries[node]]] if node in AS_countries else (0.5,0.5), 0.1)
 
-pos=nx.spring_layout(ASNs, pos=pos)
-#pos=nx.kamada_kawai_layout(ASNs, pos=pos)
-#pos=nx.spectral_layout(ASNs)
-labels = {}
+	pos=nx.spring_layout(ASNs, pos=pos)
+	labels = {}
 
-print("Assigning labels")
-for node, degree in ASNs.degree():
-	if degree > DEGREE_DISPLAY_LABEL:
-		labels[node] = node
+	if args.degree_display is not None :
+		print("Assigning labels")
+		for node, degree in ASNs.degree():
+			if degree > args.degree_display:
+				labels[node] = node
 
-print("Drawing")
-nx.draw(ASNs, pos, arrowstyle="->", with_labels=False, node_color=["r" if x in core else colors[country_continent[AS_countries[x]]] if x in AS_countries else "black" for x in ASNs.nodes()], node_size=[3*x**(2/3) for _, x in ASNs.degree()], width=0.3, edge_color="grey", alpha=0.7)
+	print("Drawing")
+	nx.draw(ASNs, pos, arrowstyle="->", with_labels=False, node_color=["r" if x in core else colors[country_continent[AS_countries[x]]] if x in AS_countries else "black" for x in ASNs.nodes()], node_size=[3*x**(2/3) for _, x in ASNs.degree()], width=0.3, edge_color="grey", alpha=0.7)
 
-print("drawing labels")
-nx.draw_networkx_labels(ASNs, pos, labels, font_size=8)
+	if args.degree_display is not None :
+		print("drawing labels")
+		nx.draw_networkx_labels(ASNs, pos, labels, font_size=8)
 
-#plt.savefig("out.png", dpi=DPI)
+	if args.output_file is not None:
+		plt.savefig(args.output_file, dpi=args.dpi)
 
-plt.show()
+	plt.show()
 
