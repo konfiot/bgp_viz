b705dc9ab0cf4c4498322ae58576eb54b0fcdc4b
Arthur Toussaint <arthurtoussaint@free.fr>
2019-11-11 14:18:19 -0500

    Add parameters to filter project and collector

--- collect/past.py
+++ collect/past.py
@@ -13,10 +13,15 @@ def do_collection(args):
 	stream = BGPStream()
 	rec = BGPRecord()
 
-	# Consider Route Views Singapore only
-	#stream.add_filter('collector','route-views.sg')
+	if args.collector != None:
+		if isinstance(args.collector, str):
+			stream.add_filter('collector', args.collector)
+		elif isinstance(args.collector, list):
+			for collector in args.collector:
+				stream.add_filter('collector', collector)
 
-	stream.add_filter('project','routeviews')
+	if args.project != None:
+		stream.add_filter('project', args.project)
 
 	# Consider RIBs dumps only
 	#stream.add_filter('record-type','ribs')
@@ -37,37 +42,37 @@ def do_collection(args):
 			print("Invalid record", rec.project, rec.collector, rec.type, rec.time, rec.status)
 			continue
 
+		elem = rec.get_next_elem()
+		while(elem):
+			news = set() # Newly announced routes
+			withdrawn = set()
+			if elem.type == "W":
+				withdrawn = {elem.fields["prefix"]} # Withdrawn routes
+
+			elif elem.type in ["A", "R"]:
+				news = {elem.fields["prefix"]}
+
+			if "as-path" in elem.fields.keys() and len(elem.fields["as-path"]) > 1: #TODO : Gérer mieux le cas à 1 dans le path, voire 0
+				path_raw = elem.fields["as-path"].split(" ")
+				path = []
+				for AS in path_raw:
+					AS = ast.literal_eval(AS)
+					if isinstance(AS, int):
+						path.append(AS)
+					else:
+						for real_AS in AS:
+							path.append(real_AS)
+
+				for i, AS in enumerate(path) :
+					# Add new routes
+						if i < (len(path) - 1):
+							neighbor = path[i+1]
+							update_graph(ASNs, AS, neighbor, news, withdrawn)
+
 			elem = rec.get_next_elem()
-			while(elem):
-				news = set() # Newly announced routes
-				withdrawn = set()
-				if elem.type == "W":
-					withdrawn = {elem.fields["prefix"]} # Withdrawn routes
 
-				elif elem.type in ["A", "R"]:
-					news = {elem.fields["prefix"]}
-
-				if "as-path" in elem.fields.keys() and len(elem.fields["as-path"]) > 1: #TODO : Gérer mieux le cas à 1 dans le path, voire 0
-					path_raw = elem.fields["as-path"].split(" ")
-					path = []
-					for AS in path_raw:
-						AS = ast.literal_eval(AS)
-						if isinstance(AS, int):
-							path.append(AS)
-						else:
-							for real_AS in AS:
-								path.append(real_AS)
-
-					for i, AS in enumerate(path) :
-						# Add new routes
-							if i < (len(path) - 1):
-								neighbor = path[i+1]
-								update_graph(ASNs, AS, neighbor, news, withdrawn)
-
-				elem = rec.get_next_elem()
-
-			# Save
-			if rec.time - last_time_saved >= args.save_rate*60:
-				save_process = multiprocessing.Process(target=save_graph, args=(ASNs.copy(), args.output_folder))
-				save_process.start()
-				last_time_saved = rec.time
+		# Save
+		if rec.time - last_time_saved >= args.save_rate*60:
+			save_process = multiprocessing.Process(target=save_graph, args=(ASNs.copy(), args.output_folder))
+			save_process.start()
+			last_time_saved += args.save_rate*60
